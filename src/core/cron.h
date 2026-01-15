aca3f62a907e10c42c5e76a12882a878d6ac7bbd
Soumya Kanti Sar <11752260+sksar@users.noreply.github.com>
2026-01-15 12:39:40 +0530

    Add log messages for cron start and stop execution

--- src/core/cron.h
+++ src/core/cron.h
@@ -9,11 +9,13 @@
 //----------------------------------------------------------------
 
 void cron_stop() {
+    LOG(LL_INFO, ("Executing cron stop"));
     if (pump_running()) return;
     pump_stop();
 }
 
 void cron_start() {
+    LOG(LL_INFO, ("Executing cron start"));
     if (pump_running()) return;
     if (!mgos_sys_config_get_app_cron_start_enable()) return;
     pump_start(mgos_sys_config_get_app_cron_start_duration());
